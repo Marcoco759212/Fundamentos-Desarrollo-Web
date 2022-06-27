71408f06757c44d1bc4cbf84dedeb5760d0f94a1
Marcoco759212 <marcoco.mahr@gmail.com>
2022-06-27 01:01:28 -0500

    Se realizan los ejercicion 1 y 2 de javascript

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,2 +1,5 @@
-'use strict'
-
+//Ejercicio 1 puede conducir o no
+const puedeConducir = (nombre, edad) => {
+	return `${nombre} tiene ${edad} de edad, ${edad < 18 ? 'no' : 'si'} puede conducir`
+}
+console.log(puedeConducir('Juán', 17))
--- ejercicio2.js
+++ ejercicio2.js
@@ -1,2 +1,14 @@
-'use strict'
-
+//ejercicio 2 convertidor de grados
+const convert = (grados, unidad) => {
+    let unit = unidad.toLowerCase();
+    if(unit !== 'celsius' &&  unit !== 'fahrenheit'){
+        return 'Error al ingresar la unidad de medida de los grados';
+    }
+    if(unit === 'celsius'){
+      return `La conversion de ${grados} grados ${unidad} es: ${(grados * 9 / 5) + 32} grados Fahrenheit`;
+    }
+    if(unit === 'fahrenheit'){
+      return `La conversion de ${grados} grados ${unidad} es: ${(grados - 32) * 5 / 9} grados Celsius`;
+    }
+  }
+  console.log(convert(78.8,'FAHRENHEIT'));
