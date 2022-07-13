bbf83eed5fb41ed6028d76e9a56800ce9a9dbf72
Martin Melo <martin.melo.dev.97@gmail.com>
2022-07-12 21:02:41 -0500

    App terminada y con refactor de código

--- main.js
+++ main.js
@@ -1,57 +1,108 @@
-const taskInput = document.querySelector('#todo-input')
-const addButton = document.querySelector('#add-todo')
-const taskListUI = document.querySelector('#list-todo')
+// == Get DOM elements ============================================================
+const taskDescInput = document.querySelector('#todo-input')
+const addTaskButton = document.querySelector('#add-todo')
+const tasksContainer = document.querySelector('#list-todo')
 
+// == Global variable =============================================================
 const tasksList = []
 
-const fillTaskList = () => {
-  taskListUI.innerHTML = ''
-
-  tasksList.forEach(taskList => {
-    const newItem = document.createElement('li')
-    const spanItem = document.createElement('span')
-    const buttonItem = document.createElement('button')
-
-    spanItem.textContent = taskList.description
-    buttonItem.textContent = '✓'
-    buttonItem.classList.add('complete-task-button')
-
-    buttonItem.setAttribute('data-index', taskList.id)
-
-    if (taskList.isCompleted === true) {
-      spanItem.style = 'text-decoration:line-through;'
-    } else {
-      spanItem.style = 'text-decoration:none;'
-    }
-
-    newItem.appendChild(spanItem)
-    newItem.appendChild(buttonItem)
-
-    taskListUI.appendChild(newItem)
-  })
+// == Functions ===================================================================
+// -- Event Listener Handlers -----------------------------------------------------
+const handleAddTask = () => {
+  const taskValue = getInputValue(taskDescInput)
+  addTask(taskValue)
+  renderTasks()
+  resetInputValue(taskDescInput)
 }
 
+// -- Utils -----------------------------------------------------------------------
+const clearNodeChilds = (node) => {
+  node.innerHTML = ''
+}
 
-addButton.addEventListener('click', () => {
-  const taskValue = taskInput.value
-  tasksList.push({
-    id: String(Math.random() * 1000000),
-    description: taskValue,
-    isCompleted: false
-  })
+const resetInputValue = (inputNode) => {
+  inputNode.value = ''
+}
 
-  fillTaskList()
+const getInputValue = (inputNode) => inputNode.value
 
-  taskInput.value = ''
+const generateRandomId = () => String(Math.random() * 1000000)
 
-  const completeTaskButtons = document.querySelectorAll('.complete-task-button')
+// -- Task - based ----------------------------------------------------------------
+const addTask = (description, isCompleted = false) => {
+  tasksList.push({ id: generateRandomId(), description, isCompleted })
+}
 
-  completeTaskButtons.forEach(buttonNode => {
-    buttonNode.addEventListener('click', (event) => {
-      const taskId = event.srcElement.getAttribute('data-index')
-      const task = tasksList.find((task) => task.id === taskId)
-      task.isCompleted = true
-      fillTaskList()
-    })
-  });
+const toggleCompleteTask = (taskId) => {
+  const task = tasksList.find((taskList) => taskList.id === taskId)
+  task.isCompleted = !task.isCompleted
+}
+
+const deleteTask = (taskId) => {
+  const taskIndex = tasksList.findIndex((taskList) => taskList.id === taskId)
+  tasksList.splice(taskIndex, 1)
+}
+
+const isTaskCompleted = (task) => task.isCompleted
+
+// -- DOM manipulation ------------------------------------------------------------
+const renderTasks = () => {
+  clearNodeChilds(tasksContainer)
+  tasksList.forEach(generateTaskElements)
+}
+
+const generateTaskElements = (task) => {
+  const listItemElement = generateListItemElement()
+  const spanElement = generateSpanElement(task)
+  const completeTaskButtonElement = generateCompleteTaskButtonElement(task)
+  const deleteTaskButtonElement = generateDeleteTaskButtonElement(task)
+  listItemElement.append(spanElement, completeTaskButtonElement, deleteTaskButtonElement)
+  tasksContainer.appendChild(listItemElement)
+}
+
+const generateListItemElement = () => {
+  const listItemElement = document.createElement('li')
+  return listItemElement
+}
+
+const generateSpanElement = (task) => {
+  const spanElement = document.createElement('span')
+  spanElement.textContent = task.description
+  spanElement.style = `text-decoration:${isTaskCompleted(task) ? 'line-through' : 'none'};`
+  return spanElement
+}
+
+const generateCompleteTaskButtonElement = (task) => {
+  const completeTaskButtonElement = document.createElement('button')
+  completeTaskButtonElement.setAttribute('data-index', task.id)
+  completeTaskButtonElement.textContent = isTaskCompleted(task) ? '🔴' : '⚪️'
+  addEventListenerToButton(completeTaskButtonElement, 'complete')
+  return completeTaskButtonElement
+}
+
+const generateDeleteTaskButtonElement = (task) => {
+  const deleteTaskButtonElement = document.createElement('button')
+  deleteTaskButtonElement.textContent = '🗑'
+  deleteTaskButtonElement.setAttribute('data-index', task.id)
+  addEventListenerToButton(deleteTaskButtonElement, 'delete')
+  return deleteTaskButtonElement
+}
+
+// -- Event Listeners -------------------------------------------------------------
+addTaskButton.addEventListener('click', () => {
+  handleAddTask()
 })
+
+taskDescInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') handleAddTask()
+})
+
+const addEventListenerToButton = (button, action) => {
+  button.addEventListener('click', (event) => {
+    const taskId = event.target.getAttribute('data-index')
+    if (action === 'complete') toggleCompleteTask(taskId)
+      if (action === 'delete')
+        if (confirm('¿Estás seguro de eliminar esta tarea?')) deleteTask(taskId)
+    renderTasks()
+  })
+}
